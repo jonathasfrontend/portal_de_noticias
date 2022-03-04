aafb52c34c59c5958d42c99eb90aa3cbdbfbeb0f
Jonathas <jonathas5678@gmail.com>
2022-03-04 16:25:24 -0300

    portaldenoticias

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 var bodyParser = require('body-parser')
+var Posts = require('./Posts.js');
+var session = require('express-session');
 const mongoose = require('mongoose');
 const path = require('path');
-var Posts = require('./Posts.js');
+const fs = require('fs');
 
 const app = express();
 
@@ -11,10 +14,18 @@ mongoose.connect('mongodb+srv://root:dFrPbwloK4qEAnKy@cluster0.xvdlp.mongodb.net
 }).catch(function(err){
     console.log(err.message)
 });
+
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : path.join(__dirname, 'temp')
+}));
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+
+app.use(session({ secret: '8j5y98jhnbujnbljbunfsjnrt9uhnitjgjgnb', cookie: { maxAge: 60000 }}))
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -104,6 +115,84 @@ app.get('/:slug',(req,res)=>{
         }
     })
 })
+
+var usuarios = [
+    {
+        nome: 'Jonathas',
+        senha: 'Jonathass001'
+    }
+]
+
+app.post('/admin/login',(req,res)=>{
+    usuarios.map(function(val){
+        if(val.nome == req.body.login && val.senha == req.body.senha){
+            req.session.login = 'Jonathas'
+        }
+    })
+    res.redirect('/admin/login')
+})
+
+app.post('/admin/cadastro',(req,res)=>{
+
+let formato = req.files.arquivo.name.split('.');
+let formato2 = req.files.arquivocapa.name.split('.');
+var imagem = "";
+var imagemcapa = ""
+
+if(formato[formato.length - 1] == 'jpg'){
+    imagem = new Date().getTime()+'.jpg';
+    req.files.arquivo.mv(__dirname+'/public/images/upload/'+imagem);
+}else{
+    fs.unlinkSync(req.files.arquivo.tempFilePath);
+}
+
+if(formato2[formato2.length - 1] == 'jpg'){
+    imagemcapa = new Date().getTime()+'.jpg';
+    req.files.arquivocapa.mv(__dirname+'/public/images/capa/'+imagemcapa);
+}else{
+    fs.unlinkSync(req.files.arquivocapa.tempFilePath);
+}
+
+    Posts.create({
+    titulo: req.body.titulo_noticia,
+    imagem: 'http://localhost:3000/public/images/upload/'+imagem,
+    categoria: req.body.categoria,
+    conteudo: req.body.noticia,
+    slug: req.body.slug,
+    author: req.body.autor,
+    vews: 0,
+    imagemCapa: 'http://localhost:3000/public/images/capa/'+imagemcapa,
+})
+res.redirect('/admin/login');
+})
+
+app.get('/admin/deletar/:id',(req,res)=>{
+    Posts.deleteOne({_id:req.params.id}).then(function(){
+        res.redirect('/admin/login');
+    })
+})
+
+app.get('/admin/login',(req,res)=>{
+
+    if(req.session.login == null){
+        res.render('admin-login');
+    }else{
+        Posts.find({}).sort({'_id': -1}).exec(function(err,posts){
+            posts = posts.map(function(val){
+                    return {
+                        id: val._id,
+                        titulo: val.titulo,
+                        conteudo: val.conteudo,
+                        descricaoCurta: val.conteudo.substr(0,100),
+                        imagem: val.imagem,
+                        slug: val.slug,
+                        categoria: val.categoria
+                    }
+            })
+            res.render('admin-painel',{posts:posts});
+        })
+    }
+});
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log('server rodando!');
